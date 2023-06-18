5b9d1742fa7f54165fd7010e2b4f5d9897d215d7
Sandeepbenny <sandeep@sanduchackonal@gmail.com>
2023-06-18 16:48:21 +0530

    refacror

--- engine/willoughby_engine.py
+++ engine/willoughby_engine.py
@@ -1,9 +1,6 @@
 from abc import ABC
 
-from car import Car
-
-
-class WilloughbyEngine(Car, ABC):
+class WilloughbyEngine(ABC):
     def __init__(self, last_service_date, current_mileage, last_service_mileage):
         super().__init__(last_service_date)
         self.current_mileage = current_mileage
@@ -11,3 +8,11 @@ class WilloughbyEngine(Car, ABC):
 
     def engine_should_be_serviced(self):
         return self.current_mileage - self.last_service_mileage > 60000
+
+class Car:
+    def __init__(self, last_service_date):
+        self.last_service_date = last_service_date
+
+    def service_car(self):
+        # Perform car servicing logic here
+        pass
